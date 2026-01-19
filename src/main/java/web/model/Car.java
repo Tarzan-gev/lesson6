a4edae18fb55180e9c52801b5cd15b00fb55d3da
tarzan <gevorgyan.tarzan@mail.ru>
2026-01-20 02:01:53 +0300

    изменение

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -1,4 +1,29 @@
 package web.model;
 
 public class Car {
+
+    private String model;
+    private String color;
+    private double engine;
+
+
+
+    public Car(String model, String color, double engine ) {
+        this.model = model;
+        this.color = color;
+        this.engine = engine;
+
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getEngine() {
+        return engine;
+    }
 }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,4 +1,32 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
 public class CarService {
+
+    private final List<Car> carList = new ArrayList<>();
+
+    public CarService() {
+        carList.add(new Car("Mercedes", "Белый", 3.5));
+        carList.add(new Car("BMW", "Чёрный", 3.0));
+        carList.add(new Car("Toyota", "Синий", 2.0));
+        carList.add(new Car("Bentley", "Серебряный", 6.0));
+        carList.add(new Car("Bugatti", "Оранжевый", 8.0));
+    }
+
+
+    public List<Car> getCars(int count) {
+        if (count >= carList.size() || count == 0) {
+            return carList;
+        }
+        return carList.stream().limit(count).collect(Collectors.toList());
+    }
 }
